b2d935f2bcd53c747d1f04658d030f30966a5c0a
gtg7784 <tae.gun7784@gmail.com>
2021-02-06 01:42:09 +0900

    feat: add flask app for render index.html & basic setup

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    result = request.form
+    return {}
+
+if __name__ == '__main__':
+    app.run(debug=True)
