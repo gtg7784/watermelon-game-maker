473f4fc586bfe6d382985aa27312b5980d7c43a3
gtg7784 <tae.gun7784@gmail.com>
2021-02-07 00:38:09 +0900

    feat: upload endpoint

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
+from utils import save_img, make_game
+from vercel import deploy
 
 app = Flask(__name__)
 
@@ -8,8 +10,19 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    result = request.form
-    return {}
+    uuid = request.headers.get('uuid')
+    email = request.form.get('email')
+    data = request.files
+    path = save_img(uuid, data)
+    completed = make_game(path, email)
+
+    if not completed:
+        abort(500)
+    
+    url = deploy(email, uuid)
+    return {
+        'url': url
+    }
 
 if __name__ == '__main__':
     app.run(debug=True)
